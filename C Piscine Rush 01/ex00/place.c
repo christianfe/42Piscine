ec15af360ce9c2256d480b38b89d9e14a25091cb
Christian Felicione <cfelicio@e3r1p8.42roma.it>
2020-10-31 22:47:01 +0100

    rush

--- C Piscine Rush 01/ex00/place.c
+++ C Piscine Rush 01/ex00/place.c
@@ -17,6 +17,7 @@ int		check_data(char *data);
 int		check_opposite(char *arr, int n, char to_find);
 void	ft_putstr(char *str);
 char	get_x(char *data, int i, int x, int type);
+void	ft_check_colrow(char *data, int *change);
 
 int		ft_adrr(int space, int i)
 {
@@ -70,13 +71,13 @@ int		ft_place_data(char *data, char *ris)
 				change = 1;
 			ris[ft_adrr(1, i_d)] = '1';
 		}
-		if (data[i_d] == '3' && check_opposite(data, i_d, '1') && ris[ft_adrr(0, i_d)] == '1' && ris[ft_adrr(3, i_d)] == '4')
+		/*if (data[i_d] == '3' && check_opposite(data, i_d, '1') && ris[ft_adrr(0, i_d)] == '1' && ris[ft_adrr(3, i_d)] == '4')
 		{
 			if (ris[ft_adrr(1, i_d)] != '3' && ris[ft_adrr(2, i_d)] != '2')
 				change = 1;
 			ris[ft_adrr(1, i_d)] = '3';
 			ris[ft_adrr(2, i_d)] = '2';
-		}
+		}*/
 		if (data[i_d] == '3' && check_opposite(data, i_d, '2') && ris[ft_adrr(0, i_d)] == '2' && ris[ft_adrr(2, i_d)] == '4')
 		{
 			if (ris[ft_adrr(1, i_d)] != '3' && ris[ft_adrr(3, i_d)] != '1')
@@ -112,13 +113,23 @@ int		ft_place_data(char *data, char *ris)
 			ris[ft_adrr(0, i_d)] = '1';
 			ris[ft_adrr(1, i_d)] = '2';
 		}
-		if (data[i_d] == '3' && check_opposite(data, i_d, '1') && ris[ft_adrr(3, i_d)] == '4' && ris[ft_adrr(2, i_d)] == '1')
+		/*if (data[i_d] == '3' && check_opposite(data, i_d, '1') && ris[ft_adrr(3, i_d)] == '4' && ris[ft_adrr(2, i_d)] == '1')
 		{
 			if (ris[ft_adrr(0, i_d)] != '2' && ris[ft_adrr(1, i_d)] != '3')
 				change = 1;
 			ris[ft_adrr(0, i_d)] = '2';
 			ris[ft_adrr(1, i_d)] = '3';
+		}*/
+////*
+		printf("%c", ris[ft_adrr(3, i_d)]);
+		if (data[i_d] == '3' && check_opposite(data, i_d, '1') && ris[ft_adrr(1, i_d)] == '1' && ris[ft_adrr(3, i_d)] == '4'  && get_x(data, i_d, 2, 1) == '2')
+		{
+			if (ris[ft_adrr(0, i_d)] != '2' && ris[ft_adrr(2, i_d)] != '3')
+				change = 1;
+			ris[ft_adrr(0, i_d)] = '2';
+			ris[ft_adrr(2, i_d)] = '3';
 		}
+///*/
 		if (data[i_d] == '1' && check_opposite(data, i_d, '2') && ris[ft_adrr(0, i_d)] == '4')
 		{
 			if (ris[ft_adrr(3, i_d)] != '3')
@@ -131,6 +142,7 @@ int		ft_place_data(char *data, char *ris)
 				change = 1;
 			ris[ft_adrr(2, i_d)] = '2';
 		}
+		ft_check_colrow(ris, &change);
 		if (change == 1)
 			i_d = 0;
 		else 
--- C Piscine Rush 01/ex00/places.c
+++ C Piscine Rush 01/ex00/places.c
@@ -13,39 +13,86 @@
 int check_zero(char *vet)
 {
 	int i;
+	int ris;
 	
 	i = 0;
+	ris = -1;
 	while (i < 4)
 	{
-		if (vet[i] == 0)
-			return (i);
+		if (vet[i] == 0 )
+		{
+			if (ris == -1)
+				ris = i;
+			else
+			ris = -2;
+		}
 		i++;
 	}
-	return (-1);
+	return (ris);
 }
 
 char	value_buco(char *array, int i, int pos)
 {
-	
+	int vet[4];
+	int k;
+
+	k = 0;
+	while (k < 4)
+	{
+		vet[(array[i + (4 * pos)]) - '1'] = 1;
+		k++;
+	}
+	if (vet[0] == 0)
+		return ('1');
+	else if (vet[1] == 0)
+		return ('2');
+	else if (vet[2] == 0)
+		return ('3');
+	else if (vet[3] == 0)
+		return ('4');
+	else
+		return (0);
 }
 
-void	check_colrow(char *data, int *change)
+void	ft_check_colrow(char *data, int *change)
 {
 	int i;
-	char vet[];
+	char vet[4];
 
 	i = 0;
 	while (i < 4)
 	{
-		vet[0] = ris[i];
-		vet[1] = ris[i + 4];
-		vet[2] = ris[i + 8];
-		vet[3] = ris[i + 12];
+		vet[0] = data[i];
+		vet[1] = data[i + 4];
+		vet[2] = data[i + 8];
+		vet[3] = data[i + 12];
 		if (check_zero(&vet[0]) >= 0)
 		{
-			vet[check_zero(&vet[0])] = value_buco(data, i, check_zero(&vet[0]));
+			data[i + (4 * check_zero(&vet[0]))] = value_buco(data, i, check_zero(&vet[0]));
+			*change = 1;
 		}
 		i++;
+		vet[0] = 0;
+		vet[1] = 0;
+		vet[2] = 0;
+		vet[3] = 0;
 	}
-		if (ris [0] == ris [])
+	i = 8;
+	while (i < 12)
+	{
+		vet[0] = data[(i - 8) * 4];
+		vet[1] = data[((i - 8) * 4) + 1];
+		vet[2] = data[((i - 8) * 4) + 2];
+		vet[3] = data[((i - 8) * 4) + 3];
+		if (check_zero(&vet[0]) >= 0)
+		{
+			data[i + (4 * check_zero(&vet[0]))] = value_buco(data, i, check_zero(&vet[0]));
+			*change = 1;
+		}
+		i++;
+		vet[0] = 0;
+		vet[1] = 0;
+		vet[2] = 0;
+		vet[3] = 0;
+	}
 }
