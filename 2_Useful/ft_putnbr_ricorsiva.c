787fd802c53f0f8c22f99d52b969a1e7f1e41ff1
christian <christian.felicione@gmail.com>
2020-11-02 13:16:27 +0100

    42

---
+++ 2_Useful/ft_putnbr_ricorsiva.c
@@ -0,0 +1,8 @@
+#include <unistd.h>
+
+void	ft_putnbr(int n)
+{
+	if (n > 9)
+		ft_putnbr(n / 10);
+	write(1, &"0123456789"[n % 10], 1);
+}
---
+++ C Piscine C 08/ex05/ft_show_tab.c
@@ -0,0 +1,34 @@
+#include <unistd.h>
+
+void	ft_putstr(char *str)
+{
+	while (*str)
+	{
+		write(1, str, 1);
+		str++;
+	}
+}
+
+void	ft_putnbr(int n)
+{
+	if (n > 9)
+		ft_putnbr(n / 10);
+	write(1, &"0123456789"[n % 10], 1);
+}
+
+void ft_show_tab(struct s_stock_str *par)
+{
+	int i;
+
+	i = 0;
+	while (par[i].str != 0)
+	{
+		ft_putstr(par[i].str);
+		ft_putstr("\n");
+		ft_putnbr(par[i].size);
+		ft_putstr("\n");
+		ft_putstr(par[i].copy);
+		ft_putstr("\n");
+		i++;
+	}
+}
