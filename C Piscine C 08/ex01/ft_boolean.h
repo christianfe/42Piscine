b01d9a284f907a227e9937345f55f16ff59eef57
christianfe <christian.felicione@gmail.com>
2020-11-03 21:40:54 +0100

    42

--- C Piscine C 08/ex01/ft_boolean.h
+++ C Piscine C 08/ex01/ft_boolean.h
@@ -20,13 +20,13 @@ typedef int	t_bool;
 # define TRUE 1
 # define FALSE 0
 # define SUCCESS 0
+# define EVEN even
+# define EVEN_MSG "I have an even number of arguments.\n"
+# define ODD_MSG "I have an odd number of arguments.\n"
 
-int EVEN(int number)
+int even(int number)
 {
 	return (number % 2 == 0);
 }
-
-# define EVEN_MSG "I have an even number of arguments.\n"
-# define ODD_MSG "I have an odd number of arguments.\n"
 
 #endif
