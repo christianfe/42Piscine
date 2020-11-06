4f1a94d0bbdd700c2d96394cf3e9761b8e36248f
Christian Felicione <cfelicio@e3r1p8.42roma.it>
2020-11-06 10:16:35 +0100

    42

---
+++ C Piscine C 12/ex13/ft_list_merge.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_list_merge.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cfelicio <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/06 09:59:59 by cfelicio          #+#    #+#             */
+/*   Updated: 2020/11/06 10:00:00 by cfelicio         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_list.h"
+
+void	ft_list_merge(t_list **begin_list1, t_list *begin_list2)
+{
+	t_list *begin1;
+	t_list *begin2;
+
+	begin1 = *begin_list1;
+	begin2 = *begin_list2;	
+	while (begin1->next)
+	{
+
+	}
+}
