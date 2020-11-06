b566c0c8785a172fddc106d29c1eb59f70c0896b
Christian Felicione <cfelicio@e3r1p8.42roma.it>
2020-11-06 09:59:00 +0100

    42

--- C Piscine C 12/ex12/ft_list_remove_if.c
+++ C Piscine C 12/ex12/ft_list_remove_if.c
@@ -11,10 +11,27 @@
 /* ************************************************************************** */
 
 #include "ft_list.h"
+#include <stdlib.h>
 
-void	ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)(), void (*free_fct)(void *))
+void	ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)(),
+							void (*free_fct)(void *))
 {
-	t_list *list;
+	t_list	*list_ptr;
+	t_list	*prev;
 
-	list = *begin_list;
+	list_ptr = *begin_list;
+	while (list_ptr)
+	{
+		if (cmp(list_ptr->next, data_ref) == 0)
+		{
+			prev->next = list_ptr->next;
+			free_fct(list_ptr->data);
+			free(list_ptr);
+		}
+		else
+		{
+			prev = list_ptr;
+			list_ptr = list_ptr->next;
+		}
+	}
 }
